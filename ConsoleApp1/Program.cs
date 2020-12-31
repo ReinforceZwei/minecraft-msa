fde6066b0b830fae42c81c170ff07095bca1a42e
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2020-12-31 12:11:29 +0800

    Add a try block for user test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,28 +20,34 @@ namespace ConsoleApp1
 
             var x = new XboxLive();
             var m = new MinecraftWithXbox();
-
-            var preAuth = x.PreAuth();
-
-            var login = x.UserLogin(email, password, preAuth);
-
-            var xbl = x.XblAuthenticate(login);
-
-            var xsts = x.XSTSAuthenticate(xbl);
-
-            string accessToken = m.LoginWithXbox(xsts.UserHash, xsts.Token);
-
-            bool hasGame = m.UserHasGame(accessToken);
-
-            if (hasGame)
+            try
             {
-                var profile = m.GetUserProfile(accessToken);
-                Console.WriteLine("Minecraft User Name: {0}", profile.UserName);
-                Console.WriteLine("UUID: {0}", profile.UUID);
+                var preAuth = x.PreAuth();
+
+                var login = x.UserLogin(email, password, preAuth);
+
+                var xbl = x.XblAuthenticate(login);
+
+                var xsts = x.XSTSAuthenticate(xbl);
+
+                string accessToken = m.LoginWithXbox(xsts.UserHash, xsts.Token);
+
+                bool hasGame = m.UserHasGame(accessToken);
+
+                if (hasGame)
+                {
+                    var profile = m.GetUserProfile(accessToken);
+                    Console.WriteLine("Minecraft User Name: {0}", profile.UserName);
+                    Console.WriteLine("UUID: {0}", profile.UUID);
+                }
+                else
+                {
+                    Console.WriteLine("This user doesn't have Minecraft");
+                }
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("This user doesn't have Minecraft");
+                Console.WriteLine(e.Message);
             }
 
             Console.ReadLine();
